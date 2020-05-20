3e62de193a7ec96144b9dedf6cdb3ceced440ea3
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-05-20 12:37:17 -0300

    Ef config

---
+++ ProductCatalog/Data/StoreDataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductCatalog.Models;
+
+namespace ProductCatalog.Data
+{
+    public class StoreDataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet <Category> Categories{ get; set; }
+    }
+}
